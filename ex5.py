c03f2b4f57399d748c66610daace41db52b20946
Noé Pécault <Nowp@users.noreply.github.com>
2022-04-24 21:53:56 +0200

    feat: Updated ex5.py

--- ex5.py
+++ ex5.py
@@ -7,16 +7,18 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def date_to_number(date):
+    year, month, day = date.split('-')
+    return float(day) / 100 + float(month) + float(year) / 10000
+
+
 def clean_penguins():
     file_names = ['PenguinData/adelie.csv', 'PenguinData/chinstrap.csv', 'PenguinData/gentoo.csv']
-    data_all = pd.concat((pd.read_csv(i) for i in file_names)).reset_index(drop=True)
-    irrelevant_columns = ['studyName', 'Sample Number', 'Region', 'Individual ID', 'Stage', 'Date Egg', 'Comments']
+    data_all = pd.concat((pd.read_csv(file) for file in file_names)).reset_index(drop=True)
+    irrelevant_columns = ['Clutch Completion', 'Sex', 'studyName', 'Sample Number', 'Region', 'Individual ID', 'Stage',
+                          'Comments']
 
-    dict_Clutch_Completion = {'Yes': 1, 'No': 0}
-    data_all['Clutch Completion'] = data_all['Clutch Completion'].map(dict_Clutch_Completion)
-
-    dict_Sex = {'MALE': 1, 'FEMALE': 0}
-    data_all['Sex'] = data_all['Sex'].map(dict_Sex)
+    data_all['Date Egg'] = data_all['Date Egg'].map(date_to_number)
 
     dict_Species = {'Adelie Penguin (Pygoscelis adeliae)': 0,
                     'Chinstrap penguin (Pygoscelis antarctica)': 1,
@@ -53,32 +55,33 @@ def run_classifier(classifier):
     return np.mean(f1_scores)
 
 
-# clean_penguins()
+clean_penguins()
 
 data = pd.read_csv("penguins.csv")
-
+column_names = data.columns.values
 X = data.loc[:, data.columns != 'Species'].to_numpy()
 y = data.loc[:, data.columns == 'Species'].to_numpy().ravel()
-
 K = 10
 
 plt.figure(figsize=(7, 7))
 
 # Analysis with n_estimators
-estimators = np.arange(30, 150, 10)
+estimators = np.arange(20, 100, 5)
 f1_score_per_param = np.zeros(len(estimators))
 for i, nr_estimators in enumerate(estimators):
-    f1_score_per_param[i] = run_classifier(RandomForestClassifier(n_estimators=nr_estimators))
+    f1_score_per_param[i] = run_classifier(RandomForestClassifier(n_estimators=nr_estimators, random_state=42))
 
 plt.plot(estimators, f1_score_per_param, c='g')
 plt.xlabel('number of estimators')
 plt.ylabel('Mean f1 score')
-plt.title('f1 with different number of estimators')
+plt.title('f1 score with different number of estimators')
 plt.show()
 
 # Comparison between ensemble and non-ensemble
 svc = SVC(probability=True, kernel='linear')
-rfc = RandomForestClassifier(n_estimators=50)
+rfc = RandomForestClassifier(n_estimators=40, max_depth=5, random_state=42)
+
 svc_f1 = run_classifier(svc)
 rfc_f1 = run_classifier(rfc)
+print(rfc.feature_importances_)
 print('f1 score for non ensemble: {:.3f} vs f1 score for ensemble: {:.3f}'.format(svc_f1, rfc_f1))
